2c9902158589638fb3e39b807e3b06064ac41c99
root <root@homelab.home>
2020-03-31 19:01:25 +0100

    Fix stop script

--- 98-stop-vms.sh
+++ 98-stop-vms.sh
@@ -5,5 +5,5 @@ source 00-set-vars.sh
 # Start all the VMs
 for x in lb bootstrap master-1 master-2 master-3 worker-1 worker-2 nfs
 do
-  virsh stop ${CLUSTER_NAME}-$x
+  virsh shutdown ${CLUSTER_NAME}-$x
 done
