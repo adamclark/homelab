10f5e328c94dc266a3abe1ecb8a7e066466819eb
root <root@homelab.home>
2020-03-31 11:31:06 +0100

    Fix pre-reqs script

--- 10-install-pre-reqs.sh
+++ 10-install-pre-reqs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yum -y update
-yum -y install qemu-kvm, qemu-img, libvirt, virt-install, libvirt-client, virt-manager, libvirt-devel, libvirt-daemon-kvm
-systemctl enable libvirtd
+yum -y install qemu-kvm qemu-img libvirt virt-install libvirt-client virt-manager libvirt-devel libvirt-daemon-kvm
+systemctl enable libvirtd.service
